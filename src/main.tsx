43b36f2d3c799a8876c557b037cba6d630e970a5
Vinod Yadav <Vinod.Yadav@teknopoint.local>
2025-12-21 13:58:53 +0530

    chore: update pnpm lockfile after adding vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import ErrorBoundary from "./components/common/ErrorBoundary.tsx";
-import { Analytics } from "@vercel/analytics/next";
+import { inject } from '@vercel/analytics'
+inject()
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <AppWrapper>
         <App />
-        <Analytics />
       </AppWrapper>
     </ErrorBoundary>
   </React.StrictMode>
